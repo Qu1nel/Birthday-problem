195f2c0030ee8a70cd6c96300da3c35d57b2087a
Ivan Kovach <eeiprex@gmail.com>
2024-02-09 15:37:36 +0300

    [add] Headers for `calendar.c`

---
+++ include/_date.h
@@ -0,0 +1,8 @@
+#pragma once
+#include <stdint.h>
+
+typedef struct Date {
+    int16_t year;
+    int16_t month;
+    int16_t day;
+} Date;
---
+++ include/calendar.h
@@ -0,0 +1,18 @@
+#ifndef CALENDAR_H
+#define CALENDAR_H
+#include <stdint.h>
+
+#include "_date.h"
+
+
+char* MONTHS[12] = {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
+
+extern Date* get_date(const int16_t y, const int16_t m, const int16_t d);
+extern Date* get_date_by_second(const int32_t second);
+extern Date** get_brithdays(const int num_of_brithdays);
+extern Date* get_match(Date** brithdays, size_t length);
+
+extern void free_brithdays(Date** brithdays, size_t len);
+
+
+#endif  // CALENDAR_H
